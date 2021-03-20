501f39ac8a3c12f1ce81a3593ffabf318b9bdb76
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2021-03-20 16:58:27 +0100

    Solicita permissão para usar a camera

--- App.tsx
+++ App.tsx
@@ -2,19 +2,49 @@ import { BarCodeScanner, BarCodeScannerResult } from 'expo-barcode-scanner';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-class App extends React.Component {
+interface TProps { }
 
-  onBarcodeScanned = (result: BarCodeScannerResult) => {
-    alert({
-      data: result.data,
-      type: result.type
-    })
+interface TState {
+  hasCameraPermission: boolean
+}
+
+class App extends React.Component<TProps, TState> {
+
+  constructor(props: TProps) {
+    super(props);
+    this.state = {
+      hasCameraPermission: false,
+    };
   }
 
-  render(){
+  onBarcodeScanned = (result: BarCodeScannerResult) => {
+    alert(JSON.stringify({
+      data: result.data,
+      type: result.type
+    }));
+  }
+
+  async componentDidMount() {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    this.setState({
+      ...this.state,
+      hasCameraPermission: status === 'granted'
+    });
+  }
+
+  render() {
+    if (!this.state.hasCameraPermission) {
+      return (
+        <View style={styles.container}>
+          <Text>No camera permission</Text>
+        </View>
+      );
+    }
+
     return (
-      <BarCodeScanner style={styles.container}
+      <BarCodeScanner 
         onBarCodeScanned={this.onBarcodeScanned}
+        style={StyleSheet.absoluteFillObject}
       />
     );
   }
@@ -26,6 +56,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
+
 export default App;
